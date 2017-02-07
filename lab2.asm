c63f8781b5e1127193b971afab95865bbb12d140
DavidAZuckerman <dzucker1@swarthmore.edu>
2017-02-06 19:00:18 -0500

    It removes spaces. nothing else

--- lab2.asm
+++ lab2.asm
@@ -28,24 +28,61 @@ main:
 	syscall
 	jal removeSpaces
 	# Print "You typed: " to console
-	la	$a1, spacelessString	
+	la	$a0, ($a1) # store the contents of $a1 (which is spacelessString) in $a0 
+	
+#FndOps: add $t0, $zero, $zero # i = 0
+#   FL1: add $t1, $t0, $a0 # Store the address of spacelessString[i] in $t1
+#    	lbu $t2, 0($t1) # Store inputString[i] in $t2
+#    	beq $t2, 43, L2 # if $t2 is equal to 43, then inputString[i] == '+' and we add
+#	add $t3, $s1 , $a1 # store the address of spacelessString[j] in $t3
+#	sb $t2, 0($t3) # save inputString[i] into the space for spacelessString[j]
+#	addi $s1, $s1, 1 # increment j
+ #  FL2: beq $t2, $zero, L3 # if $t2 is equal to $zero, then inputString[i] == '\0' and we are done copying
+  #  	addi $s0, $s0, 1 # increment i
+   # 	j L1 
 	li	$v0, 4
 	syscall
 	j main
 	
 removeSpaces:
-	addi $sp, $sp, -4
+#Copy the input string without copying the spaces
+	addi $sp, $sp, -8    # Make space on the stack 
+	sw $s1, 4($sp) # Save $s0 and $s1
 	sw $s0, 0($sp)
-	add $s0, $zero, $zero
-    L1: add $t1, $s0, $a0
-    	lbu $t2, 0($t1)
-	add $t3, $s0 , $a1
-	beq $t2, 32, L2
-	sb $t2, 0($t3)
-    L2: beq $t2, $zero, L3
-    	addi $s0, $s0, 1
-    	j L1
-    L3: lw $s0, 0($sp)
-    	addi $sp, $sp, 4
-    	jr $ra
+	#Set $s0 and $s1 to zero so they can be used as counters
+	add $s0, $zero, $zero # i = 0
+	add $s1, $zero, $zero # j = 0
+    L1: add $t1, $s0, $a0 # Store the address of inputString[i] in $t1
+    	lbu $t2, 0($t1) # Store inputString[i] in $t2
+    	beq $t2, 32, L2 # if $t2 is equal to 32, then inputString[i] == ' ' and we skip copying this char
+	add $t3, $s1 , $a1 # store the address of spacelessString[j] in $t3
+	sb $t2, 0($t3) # save inputString[i] into the space for spacelessString[j]
+	addi $s1, $s1, 1 # increment j
+    L2: beq $t2, $zero, L3 # if $t2 is equal to $zero, then inputString[i] == '\0' and we are done copying
+    	addi $s0, $s0, 1 # increment i
+    	j L1 
+    L3: lw $s0, 0($sp) #restore $s0 and s1
+    	lw $s1, 4($sp)
+    	addi $sp, $sp, 8 #deallocate the space on the stack
+    	jr $ra #return
+    	
+#findOperators:
+#Find the operators in the expression and store them
+#	addi $sp, $sp, -4    # Make space on the stack 
+#	sw $s0, 0($sp) #save $s0
+#	#Set $s0 to zero so it can be used as counter
+#	add $s0, $zero, $zero # i = 0
+#    L1: add $t1, $s0, $a0 # Store the address of spacelessString[i] in $t1
+#    	lbu $t2, 0($t1) # Store inputString[i] in $t2
+#    	beq $t2, 32, L2 # if $t2 is equal to 32, then inputString[i] == ' ' and we skip copying this char
+#	add $t3, $s1 , $a1 # store the address of spacelessString[j] in $t3
+#	sb $t2, 0($t3) # save inputString[i] into the space for spacelessString[j]
+#	addi $s1, $s1, 1 # increment j
+#    L2: beq $t2, $zero, L3 # if $t2 is equal to $zero, then inputString[i] == '\0' and we are done copying
+#    	addi $s0, $s0, 1 # increment i
+#   	j L1 
+#    L3: lw $s0, 0($sp) #restore $s0 and s1
+#   	lw $s1, 4($sp)
+#    	addi $sp, $sp, 8 #deallocate the space on the stack
+#    	jr $ra #return
 	
