d57d69e64948ab0c0cf67dd12c837411cadbebe6
DavidAZuckerman <dzucker1@swarthmore.edu>
2017-02-14 18:56:35 -0500

    Update lab3.asm

--- lab3.asm
+++ lab3.asm
@@ -24,7 +24,9 @@ main:
 	la	$a0, ($a2) # store the contents of $a2 (which is spacelessString) in $a0 
 	li 	$v0, 4
 	syscall
+	jal checkForUnevenParens
 	jal checkForInvalidSymbols
+	jal checkForInvalidSyntax
 	j main
 	
 removeSpaces:
@@ -48,6 +50,88 @@ removeSpaces:
     	lw $s1, 4($sp)
     	addi $sp, $sp, 8 #deallocate the space on the stack
     	jr $ra #return
+
+
+
+checkForInvalidSyntax:
+#Goes char by char through the expression (once it has had all spaces removed and has passed the check for invalid symbols)
+#Compares each char to the one immediately after it, looking for invalid orderings and combinations
+	addi $sp, $sp, -8 # Make space on the stack to save $s0 and $s1
+	sw $s1, 4($sp) 
+	sw $s0, 0($sp)
+	#set $s0 to zero and $s1 to 1 so they can be used as iterators
+	add $s0, $zero, $zero #i = 0
+	addi $s1, $zero, 1 #j = 1 (this register will be used to keep track of the next char, as we will be checking each char against the one after it
+L1B:    add $t1, $s0, $a0 # store the address of spacelessString[i] in $t1
+	lbu $t2, 0($t1) #store the data at the location spacelessString[i] in $t2
+	add $t3, $s1, $a0 #store the address of spacelessString[i+1] in $t3
+	lbu $t4, 0($t3) #store the data at the location spacelessString[i+1] in $t4
+	beq $t2, 10, exitB #if spacelessString[i] contains the newline character, leave this process
+	beq $t2, 40, operatorsAndOpenParens #if spacelessString[i] contains '(', then jump to the code that evaluates (, +, -, *, and /
+	beq $t2, 41, lettersAndClosedParens #if spacelessString[i] contains ')', then jump to the code that evaluates a-z,A-Z, and )
+	ble $t2, 47, operatorsAndOpenParens #if spacelessString[i] contains an ascii char whose value is less than or equal to 47, and the program has made it to this
+	#point without failing or branching out, the char must be one of +,*,-,or/, so brance to the code that evalues these operators
+	ble $t2, 57, digits # if the program has made it to this point, and spacelessString[i] contains a char whose value is less than or equal to 57, then
+	#it must be one of the digits from 0 to 9, so jump to the code that evaluates those
+	# if the program has made it to this point, and there are no invalid symbols, and we have not branched out of the process yet, then spacelessString[i] must
+	# be either a letter from a-z or from A-Z
+lettersAndClosedParens: 
+	beq $t4, 40, failureB #if spacelessString[i+1] is '(', then we have either x(, X(, or )(, all of which are Invalid
+	bge $t4, 48, failureB #at this point, we should have no invalid symbols, so if spacelessString[i+1] is >= 48, it must be in one of the ranges
+	#0-9, a-z, or A-Z. This means we have x0, X0, )0, xa, Xa, )a, xA, XA, or )A, all of which are Invalid 
+	j nextB #if we have not branched out, then move on to the next iteration
+digits: 
+	beq $t4, 40, failureB #if spacelessString[i+1] is '(', then we have either a digit immediately followed by (, which is Invalid
+	bge $t4, 65, failureB #if spacelessString[i+1] is greater than or equal to 65, because we have no invalid symbols, it must be an upper or lowercase letter.
+	#A digit immediately followed by a letter is Invalid
+	j nextB #if we have not branched out, then move on to the next iteration
+operatorsAndOpenParens:
+	beq $t4, 10, failureB #if spacelessString[i+1] is a newline, then we have an operator or a ( at the end of the expression, which is Invalid
+	beq $t4, 41, failureB #if spacelessString[i+1] is ), then we either have an operator oa ( immediately followed by a )
+			      #so we either have an incomplete subexpression within the parens or we have a set of empty parens. Either case is Invalid.
+	beq $t4, 42, failureB #if spacelessString[i+1] is *, then we have one of +*, -*, **, /*, or (*, all of which are Invalid.
+	beq $t4, 47, failureB #if spacelessString[i+1] is /, then we have one of +/, -/, */, //, or (/, all of which are Invalid.
+nextB:  addi $s0, $s0, 1
+	addi $s1, $s1, 1 #increment i and j by one
+	j L1B #start the next iteration
+failureB: la $a0, invalid # display the invalid input message
+	  li $v0, 4 # system call code to print a string to console
+	  syscall
+exitB:  lw $s0, 0($sp) #restore $s0 and s1
+    	lw $s1, 4($sp)
+    	addi $sp, $sp, 8 #deallocate the space on the stack
+    	jr $ra #return
+	  
+checkForUnevenParens:
+#Checks for uneven parentheses
+	addi $sp, $sp, -4    # Make space on the stack 
+	sw $s0, 0($sp) 	 # Save $s0 
+	#Set $s0  to zero so it can be used as a counter
+	add $s0, $zero, $zero # i = 0
+	add $t3, $zero, $zero #initialize $t3 to zero because we will be adding and subtracting to/from it to keep track of the parens we have seen
+    L1C: add $t1, $s0, $a0 # Store the address of spacelessString[i] in $t1
+    	lbu $t2, 0($t1) # Store the data at the spacelessString[i] in $t2
+    	bltz $t3, failureAndExitC # if at any time $t3 contains a negative value, it means we have encountered more )'s than ('s, so we must have an imbalance
+    	beq $t2, 10, exitC # if spacelessString[i] contains a newline, exit the process
+    	bne $t2, 40, L2C # branch if spacelessString[i] is not (
+    	addi $t3, $t3, 1 #if spacelessString[i] is a (, add 1 to $t3
+    	j L3C # branch to the section after the check to see if spacelessString[i] is )
+    L2C: bne $t2, 41, L3C #if spacelessString[i] is neither a ( nor a ), then jump ahead
+    	addi $t3, $t3, -1 # if spacelessString[i] isa ), subtract one from $t3
+    L3C: addi $s0, $s0, 1 # i = i +1
+    	j L1C #jump to the next iteration of the loop
+    failureAndExitC: lw $s0, 0($sp) #restore $s0
+    		     addi $sp, $sp, 4 #deallocate the space on the stack
+    		     la $a0, invalid # display the invalid input message
+		     li $v0, 4 # system call code to print a string to console
+	 	     syscall
+    		     jr $ra #return
+    exitC: bne $t3, $zero, failureAndExitC #at this point, if $t3 is not equal to zero, then we have an imbalance of parens
+    	lw $s0, 0($sp) #restore $s0
+    	addi $sp, $sp, 4 #deallocate the space on the stack
+    	jr $ra #return
+    	 
+    	 			 
 
 checkForInvalidSymbols:
 #Check the spaceless expression string for any invalid symbols
@@ -78,7 +162,7 @@ checkForInvalidSymbols:
  failure: la $a0, invalid # display the invalid input message
 	  li $v0, 4 # system call code to print a string to console
 	  syscall
- exit: lw $s0, 0($sp) #restore $s0 and s1
+ exit: lw $s0, 0($sp) #restore $s0
     	addi $sp, $sp, 4 #deallocate the space on the stack
     	jr $ra #return
 
